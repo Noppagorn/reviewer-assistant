70b9616c89090731c6a6c83f0671449098226653
Noppagorn <tongnop2540@gmail.com>
2023-08-15 23:59:38 +0700

    update name and layout

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,4 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Registering this listener when the script is first executed ensures that the
-// offscreen document will be able to receive messages when the promise returned
-// by `offscreen.createDocument()` resolves.
-
-const header = document.querySelector('p');
+const header = document.getElementById('timer');
 
 function updateTime() {
   const now = new Date();
@@ -27,4 +9,28 @@ function updateTime() {
   header.innerText = timeString;
 }
 
-setInterval(updateTime, 1000);
+let startTime;
+let intervalId;
+let isRunning = false;
+let elapsedTime = 0;
+
+function startStopwatch() {
+  if (!isRunning) {
+    isRunning = true;
+    startTime = new Date();
+    startTime = startTime - elapsedTime;
+    intervalId = setInterval(updateTime, 100);
+  } else {
+    isRunning = false;
+    clearInterval(intervalId);
+  }
+}
+
+function updateTime() {
+  const currentTime = new Date();
+  elapsedTime = currentTime - startTime;
+  const seconds = (elapsedTime / 1000).toFixed(1);
+  document.getElementById('time').textContent = seconds;
+}
+
+document.getElementById('startStopButton').addEventListener('click', startStopwatch);
